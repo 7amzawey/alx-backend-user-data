8f4bbc2486212d8f538ced30ba3d900f49b02c00
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-12 18:46:36 +0200

    doc

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,8 +23,15 @@ def before_request() -> str:
     """Befor request handler."""
     if auth is None:
         return
-    if request.path not in ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']:
-        if not auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+    if request.path not in [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/']:
+        if not auth.require_auth(
+                request.path, [
+                    '/api/v1/status/',
+                    '/api/v1/unauthorized/',
+                    '/api/v1/forbidden/']):
             return
         if auth.authorization_header(request) is None:
             abort(401)
