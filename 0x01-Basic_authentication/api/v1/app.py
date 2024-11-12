cc647adf9d3f221f16c1c89fdbe09de9414cd3bc
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-12 18:42:27 +0200

    checking

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,13 +2,33 @@
 """Route module for the API."""
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify
-from flask_cors import (CORS)
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+AUTH_TYPE = getenv('AUTH_TYPE')
+
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request() -> str:
+    if auth is None:
+        return
+    if request.path not in ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']:
+        if not auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
@@ -19,7 +39,7 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unathorized handler."""
+    """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
 
