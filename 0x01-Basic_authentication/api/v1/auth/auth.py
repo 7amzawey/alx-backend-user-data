44b61621f25750d177024e929726338fcc4b0f57
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-12 16:21:17 +0200

    check if path is correct

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return false."""
-        return False
+        if (excluded_paths is None):
+            return True
+        if path is None:
+            return True
+
+        normalized_path = path.rstrip('/')
+        normalized_excluded_paths = [p.rstrip('/') for p in excluded_paths]
+
+        if normalized_path in normalized_excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Return None."""
