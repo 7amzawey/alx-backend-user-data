15c96376ab1d7fdcd5f383005ed5e44adb002d18
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-14 22:12:39 +0200

    some updates mate

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 
 @app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def session_authentication():
+    """Athenticate based on a session_id stored in a cookie."""
     email = request.form.get('email')
     password = request.form.get('password')
 
