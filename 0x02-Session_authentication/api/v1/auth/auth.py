9d437592744e9946d4d8553709110cb958ae4d6b
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-14 13:14:12 +0200

    first commit

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Auth class module."""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Manange the API authentication."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Return false."""
+        if (excluded_paths is None):
+            return True
+        if path is None:
+            return True
+
+        normalized_path = path.rstrip('/')
+        normalized_excluded_paths = [p.rstrip('/') for p in excluded_paths]
+
+        if normalized_path in normalized_excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Return None."""
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Return None."""
+        return None
