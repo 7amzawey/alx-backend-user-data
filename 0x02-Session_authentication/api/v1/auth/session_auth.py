153c551d3e523c34c9e15cf6d952d3269e33e47f
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-14 15:35:43 +0200

    pydocstyle

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for session_auth a new authentication mechanism."""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """New mechanism for authentication."""
+
+    pass
