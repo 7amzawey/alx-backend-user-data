1eef80056ce898244bc588f2932e95019a8a152f
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-14 16:12:04 +0200

    session_id is done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """Module for session_auth a new authentication mechanism."""
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """New mechanism for authentication."""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session ID."""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
