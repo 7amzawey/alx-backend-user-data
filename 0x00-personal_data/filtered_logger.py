dce106bc36af6e5c78b31bab966760eaa4817129
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-19 14:13:11 +0200

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Module for a logger function."""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Return a log message obfuscated."""
     escaped_fields = [re.escape(word) for word in fields]
     pattern = r'(' + '|'.join(f"{word}=" for word in escaped_fields) + r')([^' + re.escape(separator) + r']+)'
