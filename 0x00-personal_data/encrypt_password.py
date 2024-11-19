ac62ae16b20aa085dd7b511f0b6d464ac02039ea
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-19 18:57:35 +0200

    hashing the password.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt  # type: ignore
 def hash_password(password: str) -> bytes:
     """Hash a password."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the hashed_password is the actual password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
