84b01e80d4b6b74dd5559be7d41e7502e624764b
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-20 19:36:40 +0200

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,3 +83,14 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(usr.id, **{'reset_token': token})
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except (NoResultFound, InvalidRequestError):
+            raise ValueError
+        new_hashed_password = _hash_password(password)
+        self._db.update_user(user.id, **{'password', new_hashed_password})
+        self._db.update_user(user.id, **{'session_id', None})
+        self._db._session.commit()
