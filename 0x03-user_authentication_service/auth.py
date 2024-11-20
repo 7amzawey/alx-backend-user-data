235b3356d74a1bad92fddb331fac3b9834919794
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-20 19:03:01 +0200

    nice

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt  # type: ignore
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 import uuid
 from typing import Union
 
@@ -77,7 +78,7 @@ class Auth:
         """Change the password."""
         try:
             usr = self._db.find_user_by(email=email)
-        except NoResultFound:
+        except (NoResultFound, InvalidRequestError):
             raise ValueError
         token = _generate_uuid()
         self._db.update_user(usr.id, **{'reset_token': token})
