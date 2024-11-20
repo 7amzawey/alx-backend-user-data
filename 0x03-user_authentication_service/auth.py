777747daa9337bf815b95874a7979c8878a353e9
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-20 17:22:48 +0200

    nice work

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,7 +56,7 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> User | None:
         """Get user form session_id."""
         if session_id is None:
             return None
