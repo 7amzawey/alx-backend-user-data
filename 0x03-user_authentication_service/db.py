10e4ddb8a6a1154763ee3462c5b12c9ef6b41db5
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-20 10:51:50 +0200

    bdna nwla3 elgaw w elgaw ywla3na

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from user import Base, User
 
 
@@ -33,3 +33,14 @@ class DB:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Return a user object based on his email."""
+        session = self._session
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise InvalidRequestError
+            user = session.query(User).filter_by(email=value).first()
+            if user is None:
+                raise NoResultFound
+        return user
