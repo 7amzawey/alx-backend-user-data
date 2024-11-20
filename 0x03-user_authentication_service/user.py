5bce1749de97af2a399f5927d1cf40d7510de857
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-20 09:00:49 +0200

    done

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,10 +9,10 @@ Base = declarative_base()
 
 class User(Base):
     """Crearte a class user for a db table named user."""
-    
+
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullabel=False)
-    session = Column(String, nullable=True)
-    reset_token = Column(String, nullabe=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
