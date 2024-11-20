5b67e448ae23d32c15dbec96d4f0501e40e8a7d0
7amzawey <mohamedhamza123467890@gmail.com>
2024-11-20 12:12:42 +0200

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
-"""building an auth class mate."""
+"""Building an auth class mate."""
 import bcrypt  # type: ignore
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Return the hashed_password."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        """Initialize method."""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Return a User."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+        return user
+
